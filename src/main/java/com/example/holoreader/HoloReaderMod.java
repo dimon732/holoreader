f6b5ba8626498693f8ac34157936f1bd92d7f8c6
dimon732 <147103668+dimon732@users.noreply.github.com>
2026-03-25 01:16:04 +0300

    Create HoloReaderMod.java

---
+++ src/main/java/com/example/holoreader/HoloReaderMod.java
@@ -0,0 +1,8 @@
+package com.example.holoreader;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HoloReaderMod implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
